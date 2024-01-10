2c79e563a27e40dd8ab24f01bec393caebd1cfb2
KI JONG BYUN <kjbyun0@gmail.com>
2024-01-10 09:54:52 -0600

    Assignment Done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    res_str = ', '.join(items)
+    l_idx = res_str.find(', ')
+    r_idx = res_str.rfind(', ')
+
+    if l_idx != -1:
+        if l_idx == r_idx:
+            res_str = res_str[:l_idx] + ' and ' + res_str[l_idx+2:]
+        else:
+            res_str = res_str[:r_idx+2] + 'and ' + res_str[r_idx+2:]
+
+    return res_str
